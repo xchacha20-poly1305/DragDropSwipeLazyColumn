4f7440d98dd8cdf8b70f1a26372d932a20d5c031
安容 <HystericalDragons@proton.me>
2025-10-01 21:01:47 +0800

    Downgrade min SDK to 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ernestoyaquello.dragdropswipelazycolumn.app"
-        minSdk = 26
+        minSdk = 21
         targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
