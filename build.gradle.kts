fe4e1f4204a1a4fcfaa9ae26204f72864574825c
安容 <HystericalDragons@proton.me>
2025-10-01 21:20:51 +0800

    Make it submodule

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,2 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.compose.compiler) apply false
-}
+// When used as a submodule, plugins are managed by the parent project
--- drag-drop-swipe-lazycolumn/build.gradle.kts
+++ drag-drop-swipe-lazycolumn/build.gradle.kts
@@ -1,10 +1,7 @@
-import com.vanniktech.maven.publish.SonatypeHost
-
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.vanniktech.maven.publish)
+    id("com.android.library")
+    id("kotlin-android")
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.20"
 }
 
 group = "com.ernestoyaquello.dragdropswipelazycolumn"
@@ -38,51 +35,9 @@ android {
 }
 
 dependencies {
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.kotlinx.collections.immutable)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-}
-
-mavenPublishing {
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
-    if (project.hasProperty("signingInMemoryKey")) {
-        signAllPublications()
-    }
-
-    coordinates(
-        groupId = group.toString(),
-        artifactId = "drag-drop-swipe-lazycolumn",
-        version = version.toString(),
-    )
-
-    pom {
-        name.set("DragDropSwipeLazyColumn")
-        description.set(
-            "Kotlin Android library for Jetpack Compose that implements a lazy column" +
-                    "with drag-and-drop reordering and swipe-to-dismiss functionality.",
-        )
-        inceptionYear.set("2025")
-        url.set("https://github.com/ernestoyaquello/DragDropSwipeLazyColumn")
-        licenses {
-            license {
-                name.set("MIT License")
-                url.set("https://opensource.org/license/mit")
-                distribution.set("https://opensource.org/license/mit")
-            }
-        }
-        developers {
-            developer {
-                id.set("ernestoyaquello")
-                name.set("Julio Ernesto Rodríguez Cabañas")
-                url.set("https://julioernesto.me/")
-            }
-        }
-        scm {
-            url.set("https://github.com/ernestoyaquello/DragDropSwipeLazyColumn/")
-            connection.set("scm:git:git://github.com/ernestoyaquello/DragDropSwipeLazyColumn.git")
-            developerConnection.set("scm:git:ssh://git@github.com/ernestoyaquello/DragDropSwipeLazyColumn.git")
-        }
-    }
+    implementation(platform("androidx.compose:compose-bom:2025.06.00"))
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.4.0")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
 }
